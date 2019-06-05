7a7e9ba6ba4b88a57f1fdd4406bec66e06086d5a
Sachin Sarkar <46164016+hakerarena@users.noreply.github.com>
2019-06-05 09:04:04 +0530

    Update checkPrime.java

--- checkPrime.java
+++ checkPrime.java
@@ -5,32 +5,31 @@ public class checkPrime
 {
 	public static void chkPrime()
 	{
-	    Scanner sc=new Scanner(System.in);
-        int x=sc.nextInt();
-        boolean flag=true;
-        if(x%2==0)
-        {
-            flag=false;
-        }
-        else
-        {
-            for(int i=2;i<=x/2;i++)
-            {
-                if(x%i==0)
-                {
-                    flag=false;
-                    break;
-                }
-                    
-            }
-        }
-        if(flag==true)
-            System.out.println("Prime");
-        else    
-            System.out.println("Not Prime");
+	    	Scanner sc=new Scanner(System.in);
+		int x=sc.nextInt();
+		boolean flag=true;
+		if(x%2==0)
+		{
+		    flag=false;
+		}
+		else
+		{
+		    for(int i=3;i<=Math.sqrt(x);i++)
+		    {
+			if(x%i==0)
+			{
+			    flag=false;
+			    break;
+			}
+		    }
+		}
+		if(flag==true)
+		     System.out.println("Prime");
+		else    
+		     System.out.println("Not Prime");
+		}
+		public static void main(String[] args) 
+		{
+			chkPrime();
+		}
 	}
-	public static void main(String[] args) 
-	{
-        chkPrime();
-	}
-}
